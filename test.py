858ecbc3fc2a40b287b7554d7515aad80da32e04
Софья <Yakovchenochka.s@gmail.com>
2024-11-21 10:34:01 +0700

    test5

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ def test_main():
     assert credit.calc_ann_credit(flag=False) == 310587.06, "должно быть 310587.06"
     assert credit.calc_diff_credit(flag=False) == 310500.0, "должно быть 310500.0"
 
-    credit = main.CreditCalculator(400000, 12, 6)
+    credit = main.CreditCalculator(4000000, 12, 6)
     assert credit.calc_ann_credit(flag=False) == 414116.1, "должно быть 414116.1"
     assert credit.calc_diff_credit(flag=False) == 414000.02, "должно быть 414000.02"
 
